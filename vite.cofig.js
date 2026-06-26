5f148a8d5ae08ee96c96a5bf4ae455484f5ef676
yannickMTF <yannickmustafa@gmail.com>
2026-06-26 20:10:53 -0300

    fix: scripts vinculados correctamente desde la raiz

--- vite.cofig.js
+++ vite.cofig.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
-        detalle: "detalle.html",
+        main: resolve(__dirname, "index.html"),
+        detalle: resolve(__dirname, "detalle.html"),
       },
     },
   },
