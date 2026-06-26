f8f82daed7d5fc893bfd5ede0ab26b0d41de168c
yannickMTF <yannickmustafa@gmail.com>
2026-06-26 16:16:31 -0300

    config: preparacion limpia para vercel

--- vite.cofig.js
+++ vite.cofig.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  // 🪄 IMPORTANTE: NO va la propiedad 'base' acá, porque Vercel sirve todo desde la raíz.
   build: {
     rollupOptions: {
       input: {
