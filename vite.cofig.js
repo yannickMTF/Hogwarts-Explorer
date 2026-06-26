94577573cf3d1bd574b79876aebbfbc4f75a3163
yannickMTF <yannickmustafa@gmail.com>
2026-06-26 19:55:57 -0300

    config: ajuste final de vite para vercel sin propiedad base

--- vite.cofig.js
+++ vite.cofig.js
@@ -1,17 +1,11 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"), // Atajo para importar cosas de src usando el arroba
-    },
-  },
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        detalle: resolve(__dirname, "detalle.html"),
+        main: "index.html",
+        detalle: "detalle.html",
       },
     },
   },
