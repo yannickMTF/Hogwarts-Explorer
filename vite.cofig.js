c43f0e41b23c2647d864aa654ce3158594233860
yannickMTF <yannickmustafa@gmail.com>
2026-06-26 19:37:09 -0300

    config: optimizado vite para vercel y corregidas rutas

--- vite.cofig.js
+++ vite.cofig.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // En Vercel la base SIEMPRE debe ser '/' porque el proyecto corre en la raíz de tu dominio
   base: "/",
   build: {
     rollupOptions: {
       input: {
-        main: "index.html", // Tu página principal
-        detalle: "detalle.html", // Tu página de detalles
+        main: "index.html",
+        detalle: "detalle.html",
       },
     },
   },
