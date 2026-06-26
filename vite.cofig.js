2e179d9631e16dcacbb8b74b0286ea7b939f2d88
yannickMTF <yannickmustafa@gmail.com>
2026-06-26 20:29:28 -0300

    prueba

--- vite.cofig.js
+++ vite.cofig.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
-import { fileURLToPath } from "node:url";
-import { dirname, resolve } from "node:path";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { resolve } from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
   build: {
     rollupOptions: {
       input: {
